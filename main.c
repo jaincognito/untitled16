a850321b8c8d12ee8e17e9cf13a7d11b486bb186
jaincognito <jaincognito943@gmail.com>
2021-02-01 23:34:25 +0100

    02 zmiana metody na mnozenie

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+int Policz(int a, int b){
+    return a*b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("W drugim branchu 2*3=%d", Policz(2,3));
     return 0;
 }
